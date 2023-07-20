fb4460b75ccec8399e8b290d6d6e90a5c9459e11
wesley mauricio <wesleymauricio85@gmail.com>
2023-07-20 14:57:26 -0300

    projetoFinal

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,21 @@
 import { Request, Response } from "express";
 
+import { Pet } from "../models/pets";
+import { createMenuObjects } from "../helpers/createMenuObejcts";
+
 export const search = (req: Request, res: Response)=> {
-    //res.render
+    let query: string = req.query.q as string;
+
+    if(!query){
+        res.redirect('/');
+        return;
+    }
+
+    let list = Pet.getFromName(query);
+
+    res.render('pages/page', {
+        menu: createMenuObjects(''),
+        list,
+        query
+    })
 }
